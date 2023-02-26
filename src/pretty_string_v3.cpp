f4805a33ddb3208ecee252aa42f86c8f0ea6bb39
zanoo <dmitryellison@gmail.com>
2023-02-26 23:50:42 +0300

    pretty_string_v3.cpp

--- src/pretty_string_v3.cpp
+++ src/pretty_string_v3.cpp
@@ -8,25 +8,31 @@ int ma_test() {
     int max = 0, temp_max = 0;
     std::string str;
 
-    scanf("%d", K);
+    scanf("%d\n", &K);
 
+    /// read file
     std::getline(std::cin, str);
 
     int left, right;
     for (char letter = 'a'; letter <= 'z'; ++letter) {
         left = 0, right = 0, temp_K = K;;
 
-        while (temp_K > 0) {
+        while (right != str.size()) {
             if (str.at(right) != letter) temp_K--;
+
+            if (temp_K == 0 || right == str.size() - 1 ) { // К равен нулю --> обновить ИЛИ right достиг конца строки
+                temp_max = right - left + 1;
+                max = temp_max > max ? temp_max : max;
+                temp_K = K;
+                ++left;
+                if (right == str.size() - 1)
+                    break;
+            }
+
             ++right;
         }
 
-        temp_max = right - left + 1;
-        max = temp_max > max ? temp_max : max;
-
-        while (right != str.size() - 1) {
-            //TODO: move compact [left, right] in str
-        }
+        //TODO: move compact [left, right] in str
     }
 }
 
